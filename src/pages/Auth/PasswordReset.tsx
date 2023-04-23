2d8f8db73df53bd95b41c9cd34d533f01297a6e5
Mahmoud Bassiony <beso.beso2468@gmail.com>
2023-04-23 07:34:48 +0200

    First Commit and Basic Layout

---
+++ src/pages/Auth/PasswordReset.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+const PasswordReset = (props: Props) => {
+  return <div>PasswordReset</div>;
+};
---
+++ src/pages/Auth/SignUpPage.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const SignUpPage = (props: Props) => {
+  return <div>SignUpPage</div>;
+};
+
+export default SignUpPage;
